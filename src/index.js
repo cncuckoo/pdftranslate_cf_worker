b1bd4cc88d4d9a95c7873e5cb6e18576f382d478
lisongfeng <lsf.email@gmail.com>
2025-06-11 14:41:14 +0800

    feat　支持源语言选项

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@
 
 export default {
 	async fetch(request, env, ctx) {
-		const { ARK_API_KEY, PREORITY, OPENAI_API_KEY, OPENAI_PROXY_ENDPOINT, OPENAI_MODEL_ID, DEEPSEEK_ARK_ENDPOINT, DEEPSEEK_ARK_MODEL_ID, TURING_USERS, ALLOWED_ORIGIN, ERROR } = env;
+		const { ARK_API_KEY, PREORITY, OPENAI_API_KEY, OPENAI_PROXY_ENDPOINT, OPENAI_MODEL_ID, DEEPSEEK_ARK_ENDPOINT, DEEPSEEK_ARK_MODEL_ID, TURING_USERS, ALLOWED_ORIGIN, ERROR, SOURCE_LANGUAGES } = env;
 		const corsHeaders = {
 			"Access-Control-Allow-Origin": ALLOWED_ORIGIN,
 			"Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
@@ -31,7 +31,8 @@ export default {
 			})
 
 		// 处理请求
-		const { key, text, prompt, file_info } = await request.json();
+		const { key, text, prompt, file_info, source_language } = await request.json();
+		const language = SOURCE_LANGUAGES[source_language]
 
 		if (!key) {
 			return new Response(JSON.stringify(ERROR.NO_KEY), {
@@ -56,10 +57,10 @@ export default {
 		}
 
 		const default_prompt = `你是一位译著等身的绝世翻译高手，学冠中西、博通古今，尤其擅长技术、科普内容的翻译，译文准确、流畅，尤其在底层长上下文连贯性和术语统一性方面，你总能做到贴合语境、严谨细致，令人不禁拍案叫绝。
-1.翻译风格：你的译文不会照搬原文句式，不会逐字逐句地翻译，多数情况下会重新组织语言，做到特别简洁易懂，且文白相间、地道深刻。
-2.术语统一：你的译文会尽量使用中文术语，不会使用英文术语。人名、地名、机构名、软件名称、编程语言、技术名词等，你都会使用中文术语。
-3.输出格式：你的译文会保留原文的LaTex公式、Markdown标记、HTML标签，也不会添加原文中没有的Markdown标签。
-请将用户提交的Markdown格式的英文翻译成中文，只输出译文，不输出任何无关内容。
+1.翻译风格：译文不照搬原文句式，不逐字逐句翻译，多数情况下都会重新组织语言，做到特别简洁易懂，且文白相间、通俗易懂。
+2.术语统一：人名、地名、机构名、软件名称、编程语言、技术名词等尽量使用中文术语，如果英文简写形式更通用，则使用英文简写形式，如：AI、Web、HTML、HTTP。
+3.输出格式：译文必须保留原文本中的LaTex公式、Markdown标记、HTML标签，不额外添加任何标签和标记。
+将用户提交的Markdown格式的${language}翻译成中文，只输出译文，不输出任何无关内容。
 `;
 
 		try {
@@ -105,6 +106,7 @@ export default {
 					metadata: {
 						file_info: JSON.stringify(file_info || {}),
 						key: key,
+						source_language: source_language,
 						system_prompt: system_prompt
 					}
 				})
